6610d90886537f19c309673217544f16cd6cee38
Vinícius Gutierrez da Silva Rocha <srgutyerrez@gmail.com>
2023-01-27 00:09:54 -0300

    chore: some changes

--- mirror.sh
+++ mirror.sh
@@ -5,13 +5,9 @@ set -eu
 
 export GIT_SSH_COMMAND="ssh -v -i ~/.ssh/id_rsa -o StrictHostKeyChecking=no -l $INPUT_SSH_USERNAME"
 
-git remote add mirror "$INPUT_TARGET_REPO_URL"
+git remote add mirror $INPUT_TARGET_REPO_URL
 
-# load branchs
-git fetch
-
-# NOTE: use $INPUT_TARGET_BRANCH_NAME
-git push mirror master
+git push mirror $INPUT_TARGET_BRANCH_NAME
 
 # NOTE: Since `post` execution is not supported for local action from './' for now, we need to
 # run the command by hand.
